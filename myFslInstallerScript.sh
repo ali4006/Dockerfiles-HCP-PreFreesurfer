b66a5297024e0816a30ba811566d30a2d7ed6a2d
Lalet Scaria <laletscaria@yahoo.co.in>
2016-09-26 19:28:37 +0200

    Added untar and installation steps

--- myFslInstallerScript.sh
+++ myFslInstallerScript.sh
@@ -50,7 +50,8 @@ echo "$fslDownloadDir"
 valid=$(validURL "$url")
 echo "Valid: $valid"
 if [[ "$valid" == true ]]; then
- download "$url" "$fslDownloadDir"
+ mkdir -p "$downloadDir"
+ #download "$url" "$fslDownloadDir"
 else
  echo "File doesn't exist in server"
  exit 1
@@ -114,7 +115,8 @@ elif [[ ( "$version" == "5.0.7" && "$os" == "centos6" ) || ( "$version" == "5.0.
         echo 'http://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-5.0.7-centos6_64.tar.gz'
 elif [[ ( "$version" == "5.0.7" && "$os" == "centos5" ) ]]; then
         echo 'http://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-5.0.7-centos5_64.tar.gz'
-elif [[ ( "$version" == "5.0.8" && "$os" == "centos6" ) || ( "$version" == "5.0.8" && "$os" == "centos7" ) ]]; then        echo 'http://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-5.0.8-centos6_64.tar.gz'
+elif [[ ( "$version" == "5.0.8" && "$os" == "centos6" ) || ( "$version" == "5.0.8" && "$os" == "centos7" ) ]]; then        
+	echo 'http://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-5.0.8-centos6_64.tar.gz'
 elif [[ ( "$version" == "5.0.8" && "$os" == "centos5" ) ]]; then
         echo 'http://fsl.fmrib.ox.ac.uk/fsldownloads/oldversions/fsl-5.0.8-centos5_64.tar.gz'
 else
@@ -129,7 +131,7 @@ function validURL {
 if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then echo "true"; fi
 }
 
-# Download function downloads the file in corresponding directory
+# Download function downloads the file in corresponding directory which we pass as the second parameter.
 function download {
 downloadUrl="$1"
 fileName="$2"
@@ -137,26 +139,35 @@ echo "Downloading.."
 wget $downloadUrl -O $fileName
 }
 
-#validateMD5
-
+#validateMD5 function is used for vaidating the md5 checksum value of the downloaded file with the value obtained from the server.
 function validateMD5 {
 md5FileName="$1"
 fslFileName="$2"
-value=$(<$md5FileName)
+value=$(awk '{print $1}' "$md5FileName")
+#value=$(<"$md5FileName")
 echo "MD5"
 echo "$value"
-localmd5=$(md5sum "$md5FileName")
+localmd5=$(md5sum < "$fslFileName" | awk '{print $1}')
 echo "$localmd5"
-rm "$fileName"
+rm "$md5FileName"
+if [[ "$value" == "$localmd5" ]];then
+	untar "$fslFileName"
+else
+	echo "MD5 sums doesn't match"
+	exit 0
+fi
 }
+
+# unpack in install dir and install
+function untar {
+fslFileName="$1"
+mkdir -p "$installDir" && tar xf "$fslFileName" -C "$installDir"
+}
+
+# set the environment (check in the installer)
+# Set environment variables (run export not needed)
+export FSLDIR="$installDir"
+export PATH=$PATH:${FSLDIR}/bi#. ${FSLDIR}/etc/fslconf/fsl.sh
 
 #Function call
 myFslInstaller
-
-
-# unpack in install dir and install
-
-# set the environment (check in the installer)
-# # Set environment variables (run export not needed)
-#export FSLDIR=install_dir
-#export PATH=$PATH:${FSLDIR}/bi#. ${FSLDIR}/etc/fslconf/fsl.sh

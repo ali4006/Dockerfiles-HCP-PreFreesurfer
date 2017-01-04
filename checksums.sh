407c7f06b4a9fc441c0460b1258c81aee7911b80
Lalet Scaria <laletscaria@yahoo.co.in>
2017-01-04 17:00:05 -0500

    Updated code to search all .qsub.out.* files

--- checksums.sh
+++ checksums.sh
@@ -7,7 +7,7 @@ echo "###########################################"
 echo "############ Checksum of docker image #####"
 echo "###########################################"
 
-grep "Digest: sha256:" *.qsub.out
+grep "Digest: sha256:" .qsub.out.*
 
 echo "*******************************************"
 echo "************ Checksum of files ************"

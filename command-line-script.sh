67a991a743c216c7a4c652bf4ba238e79dbd07c1
Lalet Scaria <laletscaria@yahoo.co.in>
2017-03-22 00:00:15 -0400

    Added condition to check if reprozip flag is set

--- command-line-script.sh
+++ command-line-script.sh
@@ -18,9 +18,11 @@ fi
 REPROZIP_FLAG=false
 #If reprozip flag is set
 if [ $# -eq 3 ]; then
-  REPROZIP_FLAG=true
-  SUBJECT_FOLDER=$2
-  NAME=$3
+  if [ $1 = "-r" ]; then 
+    REPROZIP_FLAG=true
+    SUBJECT_FOLDER=$2
+    NAME=$3
+  fi
 else
   SUBJECT_FOLDER=$1
   NAME=$2
@@ -30,10 +32,10 @@ EXECUTION_DIR=exec
 BEFORE_FILE=${EXECUTION_DIR}/${SUBJECT_FOLDER}/checksums-before.txt
 AFTER_FILE=${EXECUTION_DIR}/${SUBJECT_FOLDER}/checksums-after.txt
 
-create-execution-dir.sh ${SUBJECT_FOLDER} ${EXECUTION_DIR}                               		|| die ${INITDIR} "Cannot create execution directory."
-checksums.sh ${EXECUTION_DIR}/${SUBJECT_FOLDER}  > ${BEFORE_FILE}                        		|| die ${INITDIR} "Checksum script failed."
-monitor.sh &> ${EXECUTION_DIR}/${SUBJECT_FOLDER}/monitor.txt                             		|| die ${INITDIR} "Monitoring script failed."
-cd ${EXECUTION_DIR}                                                                      		|| die ${INITDIR} "Cannot cd to ${EXECUTION_DIR}."
+create-execution-dir.sh ${SUBJECT_FOLDER} ${EXECUTION_DIR}                               		  || die ${INITDIR} "Cannot create execution directory."
+checksums.sh ${EXECUTION_DIR}/${SUBJECT_FOLDER}  > ${BEFORE_FILE}                        		  || die ${INITDIR} "Checksum script failed."
+monitor.sh &> ${EXECUTION_DIR}/${SUBJECT_FOLDER}/monitor.txt                             		  || die ${INITDIR} "Monitoring script failed."
+cd ${EXECUTION_DIR}                                                                      		  || die ${INITDIR} "Cannot cd to ${EXECUTION_DIR}."
 
 #Adding the reprozip command to trace the processing of subjects
 if [ ${REPROZIP_FLAG} = true ]; then
@@ -42,13 +44,13 @@ else
   PreFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal                || die ${INITDIR} "Pipeline failed."
 fi
 
-cd ${INITDIR}                                                                            		|| die ${INITDIR} "cd .. failed."
-checksums.sh ${EXECUTION_DIR}/${SUBJECT_FOLDER} > ${AFTER_FILE}                          		|| die ${INITDIR} "Checksum script failed."
+cd ${INITDIR}                                                                            		  || die ${INITDIR} "cd .. failed."
+checksums.sh ${EXECUTION_DIR}/${SUBJECT_FOLDER} > ${AFTER_FILE}                          		  || die ${INITDIR} "Checksum script failed."
 
 #Copying the .reprozip-trace folder in execution directory to the subject folder.
 if [ ${REPROZIP_FLAG} = true ]; then
   cp -r ${EXECUTION_DIR}/.reprozip-trace ${EXECUTION_DIR}/${SUBJECT_FOLDER}
 fi
 
-ln -s ${EXECUTION_DIR}/${SUBJECT_FOLDER} ${SUBJECT_FOLDER}-${NAME}                       		|| die ${INITDIR} "Cannot link results."
+ln -s ${EXECUTION_DIR}/${SUBJECT_FOLDER} ${SUBJECT_FOLDER}-${NAME}                       		  || die ${INITDIR} "Cannot link results."
 

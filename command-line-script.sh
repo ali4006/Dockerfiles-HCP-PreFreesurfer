9030f4c7f1f258e33bf62a2684a74f8f39d8ec95
lalet <laletscaria@yahoo.co.in>
2017-06-23 10:44:01 -0400

    Reverted back to have changes only for prefreesurfer

--- command-line-script.sh
+++ command-line-script.sh
@@ -12,50 +12,20 @@ function die {
 INITDIR=$PWD
 
 if [ $# -lt 2 ]; then
-  die ${INITDIR} "Needs 2 arguments and an optional flag. If you keep -r flag as an input, then the reprozip trace process is triggered . \n
-		  If you keep the -prefs flag, then PreFreeSurfer script is executed. \n
-		  If you keep the -fs flag, Freesurfer script is executed. Along with the [-fs] flag a license file should be input as the next parameter.\n
-		  If you keep the -pfs flag, PostFreeSurfer script is executed. \n
-		  If no optional flags are kept, then PreFreeSurfer script gets executed. \n
-		  Usage: $0 [-r] <subject_folder> <name> [-fs] /path/to/license/file or $0 [-r] <subject_folder> <name> [-pfs] or  $0 [-r] <subject_folder> <name> [-prefs] "
+  die ${INITDIR} "Needs 2 arguments and an optional flag. If you keep -r flag as an input, then the reprozip trace process is triggered . Usage: $0 [-r] <subject_folder> <name>"
 fi
 
 REPROZIP_FLAG=false
 #If reprozip flag is set
-#Only case in which the number of input parameters is 5 is when reprozip flag is set along 
-#with FreeSurfer Script and path to the license file
-if [ $# -eq 5 ]; then
+if [ $# -eq 3 ]; then
   if [ $1 = "-r" ]; then 
     REPROZIP_FLAG=true
     SUBJECT_FOLDER=$2
     NAME=$3
-    SCRIPT=$4
-    LICENSE=$5
   fi
-#In case of 4 input parameters, it can either be reprozip flag with prefreesurfer or postfreesurfer
-#or in case of freesurfer, the license file without the reprozip flag.
-elif [ $# -eq 4 ]; then
-  if [ $1 = "-r" ]; then        
-    REPROZIP_FLAG=true
-    SUBJECT_FOLDER=$2
-    NAME=$3
-    SCRIPT=$4
-  else
-    SUBJECT_FOLDER=$1
-    NAME=$2
-    SCRIPT=$3
-    LICENSE=$4
-  fi
-#Otherwise it would be the working directory, subject and flags for prefreesurfer or postfreesurfer.
 else
   SUBJECT_FOLDER=$1
   NAME=$2
-  #Making sure that the script is not freesurfer without a license file.
-  if [ $3 != "-fs" ];then
-    SCRIPT=$3
-  else
-    die ${INITDIR} "Freesurfer flag should be followed by a path to the license file as the next parameter"
-  fi
 fi
 
 EXECUTION_DIR=exec
@@ -68,22 +38,10 @@ monitor.sh &> ${EXECUTION_DIR}/${SUBJECT_FOLDER}/monitor.txt                    
 cd ${EXECUTION_DIR}                                                                      		  || die ${INITDIR} "Cannot cd to ${EXECUTION_DIR}."
 
 #Adding the reprozip command to trace the processing of subjects
-if [ ${REPROZIP_FLAG} = true && ${SCRIPT}="-prefs" ]; then
-  reprozip trace PreFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal || die ${INITDIR} "PreFreeSurfer Pipeline failed."
-elif [ ${SCRIPT} = "-fs" && ${REPROZIP_FLAG} = true ];then
-  mv ${LICENSE} ${FREESURFER_HOME}/
-  source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
-  reprozip trace FreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal    || die ${INITDIR} "FreeSurfer Pipeline failed."
-elif  [ ${SCRIPT} = "-fs" ];then
-  mv ${LICENSE} ${FREESURFER_HOME}/
-  source ${FREESURFER_HOME}/SetUpFreeSurfer.sh
-  FreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal                   || die ${INITDIR} "FreeSurfer Pipeline failed."
-elif [ ${SCRIPT} = "-pfs" && ${REPROZIP_FLAG} = true ];then
-  reprozip trace PostFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal|| die ${INITDIR} "PostFreeSurfer Pipeline failed."
-elif [ ${SCRIPT} = "-pfs" ];then
-  PostFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal               || die ${INITDIR} "PostFreeSurfer Pipeline failed."
+if [ ${REPROZIP_FLAG} = true ]; then
+  reprozip trace PreFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal || die ${INITDIR} "Pipeline failed."
 else
-  PreFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal                || die ${INITDIR} "PreFreeSurfer Pipeline failed."
+  PreFreeSurferPipelineBatch.sh --StudyFolder=$PWD --Subjlist=${SUBJECT_FOLDER} --runlocal                || die ${INITDIR} "Pipeline failed."
 fi
 
 cd ${INITDIR}                                                                            		  || die ${INITDIR} "cd .. failed."
